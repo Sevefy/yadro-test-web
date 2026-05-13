c8a90a34d2703037cbc1bbc843152ad2e0ba61ba
seveCOS <sen.lofe@ya.ru>
2026-05-13 16:22:12 +0300

    загрузка конфига из .env

--- app/config.py
+++ app/config.py
@@ -1,5 +1,7 @@
 import logging
 
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 console_handler = logging.StreamHandler()
@@ -7,8 +9,14 @@ console_handler = logging.StreamHandler()
 logger.addHandler(console_handler)
 
 #### formatter ####
-formatter = logging.Formatter(
-    "%(levelname)s: %(asctime)s %(message)s"
- )
+formatter = logging.Formatter("%(levelname)s: %(asctime)s %(message)s")
 # we need setFormatter to combine handler with handler
 console_handler.setFormatter(formatter)
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_prefix="SERVER_", env_file=".env", extra="ignore"
+    )
+    host: str
+    port: int
