573ea31d1faeae4e3e5805e38439aff31b70fd37
seveCOS <sen.lofe@ya.ru>
2026-05-13 16:18:41 +0300

    вынос обращения к внешнему api

--- app/core/load_random_users.py
+++ app/core/load_random_users.py
@@ -3,20 +3,51 @@ from typing import Any
 
 import aiohttp
 
-from app.database import AsyncSessionLocal
+from app.database import AsyncSessionLocal, get_session
 from app.models.users import UserModel
 from app.repository.users import UserRepository
 from app.config import logger
 
-async def loader_random_users():
+
+async def get_users_from_randomdatatools(count: int):
+    if count < 1:
+        return
+    
     url = "https://api.randomdatatools.ru/"
-    count = 100
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, params={"count": count}) as resp:
+            if resp.status != 200:
+                logger.error(f"API error: {resp.status}")
+                resp.raise_for_status()
+            content = await resp.text()
+            data = json.loads(content)
+            if isinstance(data, list):
+                return data
+            elif isinstance(data, dict):
+                return [data]
+            else:
+                logger.error("API returned unexpected data format")
+                raise ValueError
+    
+async def loader_random_users(count=1000):
+    if count < 1:
+        return
     logger.info("Started loader user info from randomdatatools")
-    for i in range(10):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url, params={"count": count}) as resp:
-                content = await resp.text()
-                await load_data_db(json.loads(content))
+
+    MAX_PER_REQUEST = 100
+    batch_size = min(count, MAX_PER_REQUEST)
+    
+    loaded = 0
+    while loaded < count:
+        need = min(batch_size, count - loaded)
+        try:
+            data = await get_users_from_randomdatatools(need)
+            await load_data_db(data)
+            loaded += len(data)
+            logger.info(f"Loaded {loaded}/{count} users")
+        except Exception as e:
+            logger.error(f"API request error {e}")
+            break
     logger.info("Completed loader user info from randomdatatools")
 
     
@@ -32,7 +63,7 @@ async def load_data_db(users: list[dict[str: Any]]):
         )
         for user in users
     ]
-    async with AsyncSessionLocal() as db_session:
-        await UserRepository.create_users_dump(db_session, models)
+    async with AsyncSessionLocal() as session:
+        await UserRepository.create_users_dump(session, users)
 
 
