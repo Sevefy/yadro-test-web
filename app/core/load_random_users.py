a83b559014fcf572ea4884c5311835372bc01fff
seveCOS <sen.lofe@ya.ru>
2026-05-14 18:35:03 +0300

    retry и использование httpx

--- app/core/load_random_users.py
+++ app/core/load_random_users.py
@@ -1,33 +1,56 @@
+import asyncio
 import json
 from typing import Any
 
-import aiohttp
-
+import httpx
 from app.database import AsyncSessionLocal, get_session
 from app.models.users import UserModel
 from app.repository.users import UserRepository
 from app.config import logger
+from functools import wraps
+from sqlalchemy.exc import IntegrityError
 
+def retry(func=None, max_retries: int = 3, delay_multiplier: int = 2):
+    def decorator(func):        
+        @wraps(func)
+        async def wrapper(*args, **kwargs):
+            last_exception = None
+            for attempt in range(1, max_retries + 1):
+                try:
+                    result = await func(*args, **kwargs)
+                    return result
+                except (httpx.HTTPStatusError, httpx.RequestError, json.JSONDecodeError) as e:
+                    logger.warning(f"Attempt {attempt}/{max_retries} failed {e}, retry...")
+                    delay = delay_multiplier * (2 ** (attempt - 1))     
+                    await asyncio.sleep(delay)
+                    last_exception = e
+            raise last_exception
+        return wrapper
+    if func is None:
+        return decorator
+    return decorator(func)    
 
+@retry(max_retries=5)
 async def get_users_from_randomdatatools(count: int):
     if count < 1:
         return
     
     url = "https://api.randomdatatools.ru/"
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url, params={"count": count}) as resp:
-            if resp.status != 200:
-                logger.error(f"API error: {resp.status}")
-                resp.raise_for_status()
-            content = await resp.text()
-            data = json.loads(content)
-            if isinstance(data, list):
-                return data
-            elif isinstance(data, dict):
-                return [data]
-            else:
-                logger.error("API returned unexpected data format")
-                raise ValueError
+    async with httpx.AsyncClient(timeout=60) as client:
+        resp = await client.get(url, params={"count": count})
+        if resp.status_code != 200:
+            logger.error(f"API error: {resp.status_code}")
+            resp.raise_for_status()
+            
+        data = resp.json()
+        if isinstance(data, list):
+            return data
+        elif isinstance(data, dict):
+            return [data]
+        else:
+            logger.error("API returned unexpected data format")
+            raise ValueError
+
     
 async def loader_random_users(count=1000):
     if count < 1:
@@ -45,13 +68,15 @@ async def loader_random_users(count=1000):
             await load_data_db(data)
             loaded += len(data)
             logger.info(f"Loaded {loaded}/{count} users")
+        except IntegrityError as e:
+            logger.error(f"Error load data DB {e}")
         except Exception as e:
             logger.error(f"API request error {e}")
             break
     logger.info("Completed loader user info from randomdatatools")
 
     
-async def load_data_db(users: list[dict[str: Any]]):
+async def load_data_db(users: list[dict[str, Any]]):
     models = [
         UserModel(
             first_name=user.get("FirstName"),
@@ -63,7 +88,9 @@ async def load_data_db(users: list[dict[str: Any]]):
         )
         for user in users
     ]
-    async with AsyncSessionLocal() as session:
-        await UserRepository.create_users_dump(session, users)
-
+    try:
+        async with AsyncSessionLocal() as session:
+            await UserRepository.create_users_dump(session, models)
+    except IntegrityError:
+        raise
 
